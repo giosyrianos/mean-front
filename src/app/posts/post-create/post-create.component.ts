31fd0c4fb200dba31445db5fd08d9a6d859aec51
George Syrianos <gsyrianos@pavla.gr>
2020-03-28 14:06:23 +0200

    two way binding

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.scss']
 })
 export class PostCreateComponent implements OnInit {
+  public newPost = '';
+  public enteredValue = '';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
   }
 
 }
