fe60805b58a3986fe0fd1e1c1f6bba20419dbc3f
George Syrianos <gsyrianos@pavla.gr>
2020-04-01 19:13:15 +0300

    setting basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'property', loadChildren: () => import('./modules/property/property.module').then(m => m.PropertyModule) },
+
+  {path: '', component: PostListComponent},
+  {path: 'create', component: PostCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
