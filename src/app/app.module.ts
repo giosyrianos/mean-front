38c5baccabb44eb4573ec43402de26fd95f024cb
George Syrianos <gsyrianos@pavla.gr>
2020-05-06 21:29:09 +0300

    logign and signup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,13 +19,17 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { HttpClient } from '@angular/common/http';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
