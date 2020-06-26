a704a46d19a4c211fc782e84bdb53b27875d3edd
George Syrianos <gsyrianos@pavla.gr>
2020-06-26 22:06:59 +0300

    code style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -49,7 +49,9 @@ import { AuthInterceptor } from './auth/auth-interceptor';
     MatPaginatorModule,
     HttpClientModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
