1ef080389ca09b910c4f978d1e5d72dac2bd2f58
George Syrianos <gsyrianos@pavla.gr>
2020-06-29 22:22:10 +0300

    collect data from signup form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormControl, FormBuilder, FormGroup, Validators, NgModel } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Subscription } from 'rxjs';
+
+import { mimeType } from '../../posts/post-create/mime-type.validator';
 
 @Component({
   selector: 'app-signup',
@@ -12,8 +14,17 @@ export class SignupComponent implements OnInit, OnDestroy {
   isLoading = false;
   private authStatusSub: Subscription;
 
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  isEditable = false;
+  userType = '';
+
+  selectedRole = new FormControl();
+  imgPreview: string;
+
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
@@ -22,7 +33,28 @@ export class SignupComponent implements OnInit, OnDestroy {
         this.isLoading = false;
       }
     );
+    this.firstFormGroup = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      userType: this.userType
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      firstname: [''],
+      lastname: [''],
+      skills: [''],
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mimeType]
+      })
+    });
+
   }
+
+  getSelection() {
+    this.userType = this.selectedRole.value;
+  }
+
 
   signUp(formData: NgForm) {
     if (formData.invalid) {
@@ -37,6 +69,24 @@ export class SignupComponent implements OnInit, OnDestroy {
       formData.value.userType
     );
 
+  }
+
+  onFileImported(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.secondFormGroup.patchValue({ image: file });
+    this.secondFormGroup.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imgPreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+
+  submitForm() {
+    this.firstFormGroup.patchValue({ userType: this.userType });
+    console.log('first:', this.firstFormGroup);
+    console.log('second:', this.secondFormGroup);
   }
 
   ngOnDestroy() {
