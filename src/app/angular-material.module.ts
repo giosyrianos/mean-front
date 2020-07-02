ac69af1074eced1dd64e59da9f1cf867c0321c48
parhsmaropoulos <parhsmaro13@gmail.com>
2020-07-02 21:45:59 +0300

    Skill Tags on register

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -14,6 +14,7 @@ import {
   MatDialogModule,
   MatStepperModule,
   MatChipsModule,
+  MatListModule
 } from '@angular/material';
 
 
@@ -34,7 +35,8 @@ import {
     MatDialogModule,
     MatStepperModule,
     MatCardModule,
-    MatChipsModule
+    MatChipsModule,
+    MatListModule
   ]
 })
 export class AngularMaterialModule { }
