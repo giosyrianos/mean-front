a87144a51fa385e441c7316f843c34b43400cbfd
George Syrianos <gsyrianos@pavla.gr>
2020-07-03 10:43:28 +0300

    updating post model

--- src/app/posts/post.model.ts
+++ src/app/posts/post.model.ts
@@ -5,12 +5,12 @@ export  interface Post {
   imgPath: string;
   owner: string;
   bids?: any[];
-  category: string;
-  subCategory: string;
+  category?: string;
+  subCategory?: string;
   devId?: string;
   tasks?: any[];
-  completed: boolean;
-  commented: boolean;
-  tags: any[];
-  rectags: any[];
+  completed?: boolean;
+  commented?: boolean;
+  tags?: any[];
+  rectags?: any[];
 }
